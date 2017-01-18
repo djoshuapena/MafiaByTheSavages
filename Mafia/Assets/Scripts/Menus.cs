4221e9331fa8a1f5adcfc7be510fd91e5e08b4f1
davidjpena <penad14@students.ecu.edu>
2017-01-18 15:12:48 -0500

    Added PUN to Assets/ Removed Lobby Network

--- Mafia/Assets/Scripts/Menus.cs
+++ Mafia/Assets/Scripts/Menus.cs
@@ -2,71 +2,108 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Menus : MonoBehaviour {
+    //static public bool backOn = false;
     public Canvas StartScreen;
     public Canvas LoginMenu;
     public Canvas CreateAccount;
     public Canvas MainMenu;
     public Canvas OptionCanvas;
+    //public Canvas LobbyManager;
+    //public GameObject MainPanel;
+    //public GameObject ServerList;
 
-    void Start()
+    void Awake()
     {
-        OptionCanvas.enabled = false;
-        LoginMenu.enabled = false;
-        CreateAccount.enabled = false;
-        MainMenu.enabled = false;
+        if (StartScreen == null)
+        {
+            Debug.Log("Unable to detect StartScreen");
+        }
+        if (LoginMenu == null)
+        {
+            Debug.Log("Unable to detect LoginMenu");
+        }
+        if (CreateAccount == null)
+        {
+            Debug.Log("Unable to detect CreateAccount");
+        }
+        if (MainMenu == null)
+        {
+            Debug.Log("Unable to detect MainMenu");
+        }
+        if (OptionCanvas == null)
+        {
+            Debug.Log("Unable to detect OptionCanvas");
+        }
+        //if (LobbyManager == null)
+        //{
+        //    Debug.Log("Unable to detect LobbyManager");
+        //}
+    }
+
+    public void SetMenu(Canvas theOneIWant)
+    {
+        StartScreen.gameObject.SetActive(theOneIWant == StartScreen);
+        OptionCanvas.gameObject.SetActive(theOneIWant == OptionCanvas);
+        LoginMenu.gameObject.SetActive(theOneIWant == LoginMenu);
+        CreateAccount.gameObject.SetActive(theOneIWant == CreateAccount);
+        MainMenu.gameObject.SetActive(theOneIWant == MainMenu);
+        //LobbyManager.gameObject.SetActive(theOneIWant == LobbyManager);
+    }
+
+    //public void NetworkMenu(GameObject menu)
+    //{
+    //    MainPanel.SetActive(menu == MainPanel);
+    //    ServerList.SetActive(menu == ServerList);
+    //}
+
+    public void Start()
+    {
+        SetMenu(StartScreen);
     }
     
+    public void StartScreenOn()
+    {
+        SetMenu(StartScreen);
+    }
+
     public void LoginOn()
     {
-        LoginMenu.enabled = true;
-        StartScreen.enabled = false;
+        SetMenu(LoginMenu);
     }
 
     public void CreateOn()
     {
-        CreateAccount.enabled = true;
-        StartScreen.enabled = false;
+        SetMenu(CreateAccount);
     }
 
     public void MainOn()
     {
-        MainMenu.enabled = true;
-        if (LoginMenu.enabled)
-            LoginMenu.enabled = false;
-        else if (CreateAccount.enabled)
-            CreateAccount.enabled = false;
-    }
-
-    public void Cancel()
-    {
-        StartScreen.enabled = true;
-        if (LoginMenu.enabled)
-            LoginMenu.enabled = false;
-        else if (CreateAccount.enabled)
-            CreateAccount.enabled = false;
+        SetMenu(MainMenu);
     }
 
     public void OptionsOn()
     {
-        OptionCanvas.enabled = true;
-        MainMenu.enabled = false;
+        SetMenu(OptionCanvas);
     }
 
-    public void ExitOptions()
+    public void startServerList()
     {
-        MainMenu.enabled = true;
-        OptionCanvas.enabled = false;
+        SceneManager.LoadScene(1);
     }
-    public void Logout()
-    {
-        MainMenu.enabled = false;
-        StartScreen.enabled = true;
-    }
+    //public void ListLobbyServers()
+    //{
+    //    backOn = true;
+    //    SetMenu(LobbyManager);
+    //    NetworkMenu(ServerList);
+    //}
 
-    public void StartLobby()
-    {
-        Application.LoadLevel(1);
-    }
+    //public void StartLobby()
+    //{
+    //    backOn = false;
+    //    SetMenu(LobbyManager);
+    //    NetworkMenu(MainPanel);
+    //}
 }

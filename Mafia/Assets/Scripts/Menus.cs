dc29cb3cfb44f9068547b5a83b71a38886a3145e
David Pena <penad14@students.ecu.edu>
2017-01-17 19:05:56 -0500

    Version 0.012
    Created Base Menu System

---
+++ Mafia/Assets/Scripts/Menus.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menus : MonoBehaviour {
+    public Canvas StartScreen;
+    public Canvas LoginMenu;
+    public Canvas CreateAccount;
+    public Canvas MainMenu;
+    public Canvas OptionCanvas;
+
+    void Start()
+    {
+        OptionCanvas.enabled = false;
+        LoginMenu.enabled = false;
+        CreateAccount.enabled = false;
+        MainMenu.enabled = false;
+    }
+    
+    public void LoginOn()
+    {
+        LoginMenu.enabled = true;
+        StartScreen.enabled = false;
+    }
+
+    public void CreateOn()
+    {
+        CreateAccount.enabled = true;
+        StartScreen.enabled = false;
+    }
+
+    public void MainOn()
+    {
+        MainMenu.enabled = true;
+        if (LoginMenu.enabled)
+            LoginMenu.enabled = false;
+        else if (CreateAccount.enabled)
+            CreateAccount.enabled = false;
+    }
+
+    public void Cancel()
+    {
+        StartScreen.enabled = true;
+        if (LoginMenu.enabled)
+            LoginMenu.enabled = false;
+        else if (CreateAccount.enabled)
+            CreateAccount.enabled = false;
+    }
+
+    public void OptionsOn()
+    {
+        OptionCanvas.enabled = true;
+        MainMenu.enabled = false;
+    }
+
+    public void ExitOptions()
+    {
+        MainMenu.enabled = true;
+        OptionCanvas.enabled = false;
+    }
+    public void Logout()
+    {
+        MainMenu.enabled = false;
+        StartScreen.enabled = true;
+    }
+}

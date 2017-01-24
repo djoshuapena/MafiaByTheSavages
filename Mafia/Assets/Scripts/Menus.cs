9802feb746e4787344bdbe263c5e2812a1de7c83
David Pena <penad14@students.ecu.edu>
2017-01-23 20:59:16 -0500

    Added Login
    Added login to menu

--- Mafia/Assets/Scripts/Menus.cs
+++ Mafia/Assets/Scripts/Menus.cs
@@ -10,6 +10,22 @@ public class Menus : MonoBehaviour {
     public Canvas MainMenu;
     public Canvas OptionCanvas;
 
+    //private variables
+    public InputField EnterUsername;
+    public InputField EnterPassword;
+    public InputField CreateUsername;
+    public InputField CreatePassword;
+    public InputField ConfirmPassword;
+
+    //private variables
+    private string Username = "";
+    private string Password = "";
+    private string CreateAccountUrl = "http://giramdev.000webhostapp.com/CreateAccountT.php";
+    private string LoginUrl = "http://giramdev.000webhostapp.com/LoginAccountT.php";
+    private string ConfirmPass = "";
+    private string CPassword = "";
+    private string CUsername = "";
+
     private void Awake()
     {
         if (StartScreen == null)
@@ -61,5 +77,77 @@ public class Menus : MonoBehaviour {
     public void OptionsOn()
     {
         SetMenu(OptionCanvas);
+    }
+
+    public void CreateNewAcount()
+    {
+        if (CreateUsername != null && CreatePassword != null && ConfirmPassword != null)
+        {
+            CUsername = CreateUsername.text;
+            CPassword = CreatePassword.text;
+            ConfirmPass = ConfirmPassword.text;
+            StartCoroutine(CreateNewAccount());
+        }
+    }
+
+    public void LoginToAccount()
+    {
+        if (EnterUsername != null && EnterPassword != null)
+        {
+            Username = EnterUsername.text;
+            Password = EnterPassword.text;
+            StartCoroutine(LoginAccount());
+        }
+    }
+
+    IEnumerator CreateNewAccount()
+    {
+        Debug.Log("button pressed");
+        WWWForm Form = new WWWForm();
+        Form.AddField("Username", CUsername);
+        Form.AddField("Password", CPassword);
+        WWW CreateAccountWWW = new WWW(CreateAccountUrl, Form);
+        yield return CreateAccountWWW; //wait for php
+
+        if (CreateAccountWWW.error != null)
+        {
+            Debug.LogError("Cannot connect to account Creation");
+        }
+        else
+        {
+            Debug.Log(CreateAccountWWW.text);
+            string CreateAccountReturn = CreateAccountWWW.text;
+            if (CreateAccountReturn == "Success")
+            {
+                Debug.Log("Success: Account created");
+                SetMenu(LoginMenu);
+            }
+        }
+    }
+
+    IEnumerator LoginAccount()
+    {
+        WWWForm Form = new WWWForm();
+        Form.AddField("Username", Username);
+        Form.AddField("Password", Password);
+        WWW LoginAccountWWW = new WWW(LoginUrl, Form);
+        yield return LoginAccountWWW;
+        if (LoginAccountWWW.error != null)
+        {
+            Debug.LogError("Cannot connect to Login");
+        }
+        else
+        {
+            string LogText = LoginAccountWWW.text;
+            Debug.Log(LogText);
+            string[] LogTextSplit = LogText.Split(':');
+            if (LogTextSplit[0] == "0")
+            {
+                if (LogTextSplit[1] == "Success")
+                {
+                    SetMenu(MainMenu);
+                }
+            }
+        }
     }
 }

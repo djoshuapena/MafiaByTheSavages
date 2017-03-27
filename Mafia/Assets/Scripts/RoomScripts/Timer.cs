19b532396e7fae073e4831595f5314ffc041a1bd
Dean Cahoon <cahoond14@students.ecu.edu>
2017-03-27 12:10:07 -0400

    Fixed Timer.cs
    Fixed syntax errors

--- Mafia/Assets/Scripts/RoomScripts/Timer.cs
+++ Mafia/Assets/Scripts/RoomScripts/Timer.cs
@@ -4,83 +4,74 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class WaitingRoom : Photon.PunBehaviour//MonoBehaviour
+public class Timer : Photon.PunBehaviour//MonoBehaviour
 {
-	private List<GameObject> playerNamePrefabs = new List<GameObject>();
-	
-	private PhotonView myPhotonView;
+    private List<GameObject> playerNamePrefabs = new List<GameObject>();
 
-	private int minPlayers = 2;
+    private PhotonView myPhotonView;
 
-	float timeLeft = 30.0f;
+    private int minPlayers = 2;
 
-	public Text timer;
+    float timeLeft = 30.0f;
 
-
-	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-	{
-		if (stream.isWriting == true) {
-			stream.SendNext (timeLeft);
-			if (timeLeft < 1)
-			{
-				SceneManager.LoadScene("GameScene");
-			}
-		} else {
-			timeLeft = (float)stream.ReceiveNext ();
-			ShowTime ();
-			if (timeLeft < 1)
-			{
-				SceneManager.LoadScene("GameScene");
-			}
-		}
-	}
-
-	void Start()
-	{
-		myPhotonView = gameObject.GetComponent<PhotonView>();
-
-		if (PhotonNetwork.isMasterClient == true) // host starts the countdown
-		{
-			//timeLeft = 30.0f;
-		myPhotonView.RPC("Countdown", PhotonTargets.AllBuffered, PhotonNetwork.time);
-		}
-	}
-
-	[PunRPC]
-	void Countdown(double timerStart)
-	{
-		timeLeft -= (float)(timerStart - PhotonNetwork.time);
-		StartCoroutine ("TimerCountdown");
-	}
-		
-	IEnumerator TimerCountdown()
-	{
-		while ( timeLeft > 0f )
-		{
-			yield return new WaitForEndOfFrame ();
-			if (PhotonNetwork.isMasterClient == true) {
-				timeLeft -= Time.deltaTime;
-			}
-			ShowTime ();
-		}
-
-	}
-
-	void ShowTime()
-	{
-		timer.text = "Time Left:" + Mathf.Round(timeLeft);
-	}
-		
+    public Text timer;
 
 
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting == true)
+        {
+            stream.SendNext(timeLeft);
+            if (timeLeft < 1)
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+        else
+        {
+            timeLeft = (float)stream.ReceiveNext();
+            ShowTime();
+            if (timeLeft < 1)
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+    }
 
+    void Start()
+    {
+        myPhotonView = gameObject.GetComponent<PhotonView>();
 
+        if (PhotonNetwork.isMasterClient == true) // host starts the countdown
+        {
+            //timeLeft = 30.0f;
+            myPhotonView.RPC("Countdown", PhotonTargets.AllBuffered, PhotonNetwork.time);
+        }
+    }
 
+    [PunRPC]
+    void Countdown(double timerStart)
+    {
+        timeLeft -= (float)(timerStart - PhotonNetwork.time);
+        StartCoroutine("TimerCountdown");
+    }
 
+    IEnumerator TimerCountdown()
+    {
+        while (timeLeft > 0f)
+        {
+            yield return new WaitForEndOfFrame();
+            if (PhotonNetwork.isMasterClient == true)
+            {
+                timeLeft -= Time.deltaTime;
+            }
+            ShowTime();
+        }
 
+    }
 
-
-
-
-
-
+    void ShowTime()
+    {
+        timer.text = "Time Left:" + Mathf.Round(timeLeft);
+    }
+}

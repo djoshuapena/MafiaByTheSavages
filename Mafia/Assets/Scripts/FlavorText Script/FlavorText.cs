7a58b5edfa663b469957a3d65b0e199c0a8a48a4
halea <halea13@students.ecu.edu>
2017-03-28 10:13:30 -0400

    so I can work from my laptop
    need to know what key should be for the dictionary (player prefab, etc)
    and if this script should do the filter or the php (but I really think
    it shouldn't matter for now)?

---
+++ Mafia/Assets/Scripts/FlavorText Script/FlavorText.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlavorText : MonoBehaviour {
+
+    private string flavorTextPHP = "mafiasav.com/flavorText.php";//Not there yet
+    private Dictionary<TBD, string> flavorTextDict;//TBD will probably be a player prefab
+
+
+    //allows other classes to initialize flavorTextDict
+    //when they wish to use it
+    public void InitializeFlavorTextDict()
+    {
+       Dictionary<TBD, string> flavorTextDict = new Dictionary<TBD, string>();//TBD will probably be a player prefab
+    }
+
+
+    //gets the flavor text from the dictionary
+    public string GetFlavorText()
+    {
+        KeyValuePair<TBD, string> juicyFruit = new KeyValuePair<TBD, string>();//the flavor text
+
+
+        //need to ask if I will be doing the filtering or the PHP??
+
+        /*
+        if (mafia)
+        {
+
+        }
+        if (sheriff)
+        {
+
+        }
+        if (nurse)
+        {
+
+        }
+        if (civilian)
+        {
+
+        }
+        if (dusk)
+        {
+
+        }*/
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

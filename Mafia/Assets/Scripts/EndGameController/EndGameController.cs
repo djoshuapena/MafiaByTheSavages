40d829b9a489e27cfce7033e6a49cf2a35724ab5
halea <halea13@students.ecu.edu>
2017-03-29 20:35:26 -0400

    EndGameController is mostly done. Just need to establish correct globals

---
+++ Mafia/Assets/Scripts/EndGameController/EndGameController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EndGameController : MonoBehaviour {
+    
+    //determines if the last civilian has been killed
+    public bool isLastCivilian()
+    {
+        PhotonPlayer [] playerList = PhotonNetwork.playerList;//list of players in game
+        for (int x = 0; x < playerList.Length; x++)
+        {
+            if(playerList[x].CustomProperties.ContainsKey("sheriff"))
+            {
+                return false;
+            }
+            if (playerList[x].CustomProperties.ContainsKey("nurse"))
+            {
+                return false;
+            }
+            if (playerList[x].CustomProperties.Equals("civilian"))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //determines if the last mafia has been killed
+    public bool isLastMafia()
+    {
+        PhotonPlayer[] playerList = PhotonNetwork.playerList;//list of players in game
+        for (int x = 0; x < playerList.Length; x++)
+        {
+            if (playerList[x].CustomProperties.ContainsKey("mafia"))
+            {
+                return false;
+            }
+        }
+            return true;
+    }
+
+    //determines if the last sheriff has been killed
+    public bool isLastSheriff()
+    {
+        PhotonPlayer[] playerList = PhotonNetwork.playerList;//list of players in game
+
+        for (int x = 0; x < playerList.Length; x++)
+        {
+            if (playerList[x].CustomProperties.ContainsKey("sheriff"))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}

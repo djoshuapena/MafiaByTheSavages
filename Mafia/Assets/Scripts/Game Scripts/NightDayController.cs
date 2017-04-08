0fa181406a3e9de829dde651456445b81c4e4427
David Pena <penad14@students.ecu.edu>
2017-04-08 16:37:02 -0400

    Finished Night Day Controller
    Night day controller script is done and needs to be tested.

--- Mafia/Assets/Scripts/Game Scripts/NightDayController.cs
+++ Mafia/Assets/Scripts/Game Scripts/NightDayController.cs
@@ -7,31 +7,195 @@ public class NightDayController : MonoBehaviour {
 
 	public VoteController voteController;
 	public FlavorText flavorText;
+    public GameController game;
+    public TimerGraphicsController timer;
+    public GameObject NightDayPanel;
+    public GameObject ParentPanel;
+    public GameObject BackgroundImage;
+    public Sprite NightBackground;
+    public Sprite DayBackground;
+    public Sprite Civilian;
+    public Sprite Mafia;
+    public Sprite Nurse;
+    public Sprite Sheriff;
+    public Sprite DeadCivilian;
+    public Sprite DeadMafia;
+    public Sprite DeadNurse;
+    public Sprite DeadSheriff;
 
-	public Text text;
+    public GameObject characterIconPrefab;
+    public List<GameObject> characterIconPrefabs;
+    public GameObject characterSelectedIcon;
+
+	//public Text text;
 
 	//UpdateVote(name) updates the vote of name
 	public void UpdateVote(string name){
 		voteController.ChangeVote (name);
 	}
 
-	public bool InitializeView(string nightDay){
-        return true;
-
-	}
-
-    public bool StartView(string nightDay)
+    /// <summary>
+    /// Return the sprite that represents the role of 'player'
+    /// </summary>
+    /// <param name="player">The PhotonPlayer that needs thier role sprite</param>
+    /// <returns>The sprite that represents the players role </returns>
+    private Sprite PlayerRole(PhotonPlayer player)
     {
+        if ((string)player.CustomProperties[Global.CustomProperties.Roles] == Global.Role.Mafia)
+            return Mafia;
+        else if ((string)player.CustomProperties[Global.CustomProperties.Roles] == Global.Role.Sheriff)
+            return Sheriff;
+        else if (((string)player.CustomProperties[Global.CustomProperties.Roles] == Global.Role.Nurse))
+            return Nurse;
+        else if ((string)player.CustomProperties[Global.CustomProperties.Roles] == Global.Role.Civilian)
+            return Civilian;
+        throw new System.Exception("Player Not Assigned Role");
+    }
+
+    /// <summary>
+    /// Return the dead sprite that represents the role of 'player'
+    /// </summary>
+    /// <param name="player">The PhotonPlayer that needs thier role sprite</param>
+    /// <returns>The dead sprite that represents the players role </returns>
+    private Sprite DeadPlayerRole(PhotonPlayer player)
+    {
+        if ((string)player.CustomProperties[Global.CustomProperties.Roles] == Global.Role.Mafia)
+            return DeadMafia;
+        else if ((string)player.CustomProperties[Global.CustomProperties.Roles] == Global.Role.Sheriff)
+            return DeadSheriff;
+        else if (((string)player.CustomProperties[Global.CustomProperties.Roles] == Global.Role.Nurse))
+            return DeadNurse;
+        else if ((string)player.CustomProperties[Global.CustomProperties.Roles] == Global.Role.Civilian)
+            return DeadCivilian;
+        throw new System.Exception("Player Not Assigned Role");
+    }
+
+    /// <summary>
+    /// At the very start of the game, each player in the PhotonNetwork will
+    /// build thier list of player names and icons. The local player will
+    /// be able to see the sprite that represents their role. If the local
+    /// player is a mafia, then they can see the other mafia players.
+    /// </summary>
+    /// <returns>Whether or not it works.</returns>
+    public bool StartGameInitialize()
+    {
+        for(int pos = 0; pos < PhotonNetwork.playerList.Length; pos++)
+        {
+            GameObject newPlayer = Instantiate(characterIconPrefab);
+            newPlayer.transform.SetParent(ParentPanel.transform);
+            newPlayer.GetComponentInChildren<Text>().text = (string)PhotonNetwork.playerList[pos].CustomProperties[Global.CustomProperties.Name];
+            if (newPlayer.GetComponentInChildren<Text>().text == (string)PhotonNetwork.player.CustomProperties[Global.CustomProperties.Name])
+                newPlayer.GetComponent<Image>().sprite = PlayerRole(PhotonNetwork.player);
+            else if (((string)PhotonNetwork.player.CustomProperties[Global.CustomProperties.Roles] == Global.Role.Mafia) && ((string)PhotonNetwork.playerList[pos].CustomProperties[Global.CustomProperties.Roles] == Global.Role.Mafia))
+                newPlayer.GetComponent<Image>().sprite = Mafia;
+            else
+                newPlayer.GetComponent<Image>().sprite = Civilian;
+            newPlayer.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+            characterIconPrefabs.Add(newPlayer);
+        }
+        if (characterIconPrefabs == null)
+            return false;
         return true;
     }
 
-	private void ChangeBackground(){
+    /// <summary>
+    /// Set the background image based on 'state'. If there are
+    /// any names in the 'deadPlayers' list, then disable those players,
+    /// and change their icons.
+    /// </summary>
+    /// <param name="state">Current state of the game</param>
+    /// <param name="deadPlayers">Players that will be disabled.</param>
+    /// <returns></returns>
+    /// 
+    /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+     *                  FUTURE EXTENSION
+     *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+     *  deadPlayers needs to be a list of PhotonPlayers, this needs
+     *  to be changed in VoteController. VoteController should return
+     *  list of PhotonPlayers, not strings.
+     */
+	public bool InitializeView(string state, List<string> deadPlayers)
+    {
+        if (state == Global.States.Night)
+            BackgroundImage.GetComponent<Image>().sprite = NightBackground;
+        else if (state == Global.States.Day)
+            BackgroundImage.GetComponent<Image>().sprite = DayBackground;
+        else
+            throw new System.Exception("State " + state + " not recognized by NightDayController.");
 
+        characterSelectedIcon.SetActive(false);
+
+        // If the state is night, and the player is a civilian, all buttons are disabled.
+        if((state == Global.States.Night) && ((string)PhotonNetwork.player.CustomProperties[Global.CustomProperties.Roles] == Global.Role.Civilian))
+        {
+            for(int pos = 0; pos < characterIconPrefabs.Count; pos++)
+            {
+                characterIconPrefabs[pos].GetComponent<Button>().enabled = false;
+            }
+        }
+
+        if((bool)PhotonNetwork.player.CustomProperties[Global.CustomProperties.Dead])
+        {
+            for (int pos = 0; pos < characterIconPrefabs.Count; pos++)
+            {
+                characterIconPrefabs[pos].GetComponent<Button>().enabled = false;
+            }
+        }
+
+        // David hates this stupid loop-de-loop. James seconds Davids hatred.
+        // Once you (whoever you are) chages the vote controller to be lovely
+        // Change this bulls**t.
+        if (deadPlayers != null)
+        {
+            for (int pos = 0; pos < characterIconPrefabs.Count; pos++)
+            {
+                if (deadPlayers.Contains(characterIconPrefabs[pos].GetComponentInChildren<Text>().text))
+                {
+                    characterIconPrefabs[pos].GetComponent<Button>().enabled = false;
+                    for (int player = 0; player < PhotonNetwork.playerList.Length; player++)
+                    {
+                        if ((string)PhotonNetwork.playerList[player].CustomProperties[Global.CustomProperties.Name] == characterIconPrefabs[pos].GetComponentInChildren<Text>().text)
+                        {
+                            characterIconPrefabs[pos].GetComponent<Image>().sprite = DeadPlayerRole(PhotonNetwork.playerList[player]);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
 	}
 
-	public void UpdateIcons(){
+    /// <summary>
+    /// Start the day or night panel with all objects
+    /// already initialized.
+    /// </summary>
+    /// <returns></returns>
+    public bool StartView()
+    {
+        NightDayPanel.SetActive(true);
+        StartCoroutine(checkTimer());
+        return true;
+    }
 
-	}
+    IEnumerator checkTimer()
+    {
+        yield return !timer.TimeUP();
+        NightDayPanel.SetActive(false);
+    }
+
+	public void VoteThisPlayerButton(){
+        characterSelectedIcon.GetComponent<Image>().sprite = characterIconPrefab.GetComponent<Image>().sprite;
+        characterSelectedIcon.GetComponentInChildren<Text>().text = characterIconPrefab.GetComponentInChildren<Text>().text;
+        PhotonNetwork.player.CustomProperties[Global.CustomProperties.VotedFor] = characterIconPrefab.GetComponentInChildren<Text>().text;
+        characterSelectedIcon.SetActive(true);
+    }
+
+    public void CancelVote()
+    {
+        PhotonNetwork.player.CustomProperties[Global.CustomProperties.VotedFor] = "";
+        characterSelectedIcon.SetActive(false);
+    }
+
 
 	//DisplayFlavorText(scene) displays te correct flavor text based on the scene parameter
 	/*private void DisplayFlavorText(string scene){
